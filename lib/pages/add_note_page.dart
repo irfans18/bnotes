24e77de0e5a2d6c970192131de8304e2b13c7168
irfans18 <58197653+irfans18@users.noreply.github.com>
2021-09-30 10:39:07 +0700

    add dropdown search
    update dependencies

--- lib/pages/add_note_page.dart
+++ lib/pages/add_note_page.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_final_fields
 
+import 'package:bnotes/models/category.dart';
+import 'package:bnotes/services/category_sevice.dart';
 import 'package:flutter/material.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 class AddNotePage extends StatefulWidget {
   @override
@@ -9,8 +12,30 @@ class AddNotePage extends StatefulWidget {
 
 class _AddNotePageState extends State<AddNotePage> {
   var _selectedItem;
-  var _category;
-  
+  var _categoryList = ["Brazil", "Tunisia", "Test"];
+
+  var _categoryService = CategoryService();
+
+  var _titleController = TextEditingController();
+  var _descriptionController = TextEditingController();
+  var _dateController = TextEditingController();
+  var _categoryController = TextEditingController();
+
+  // getAllCategories() async {
+  //   _categoryList.clear();
+  //   var categories = await _categoryService.readCategory();
+  //   categories.forEach((category) {
+  //     setState(() {
+  //       var categoryModel = Category();
+  //       categoryModel.name = category["name"];
+  //       categoryModel.description = category["description"];
+  //       categoryModel.id = category["id"];
+  //       _categoryList.add(categoryModel);
+  //     });
+  //   });
+  // }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,28 +48,37 @@ class _AddNotePageState extends State<AddNotePage> {
         child: ListView(
           children: <Widget>[
             TextField(
+              controller: _titleController,
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.title),
                 labelText: "Title",
                 hintText: "Write the title",
               ),
             ),
-            DropdownButtonFormField(
-              value: _selectedItem,
-              items: _category,
-              hint: Text("Category"),
-              onChanged: (value) {
-                setState(() {
-                  _selectedItem = value;
-                });
+                       
+            DropdownSearch<String>(
+              mode: Mode.MENU,
+              showSelectedItems: true,
+              showSearchBox: true,
+              // ignore: prefer_const_literals_to_create_immutables
+              items: _categoryList,
+              label: "Category",
+              hint: "Select Category",
+              onChanged: (val) {
+                _selectedItem = val;
+                debugPrint(_selectedItem);
               },
-              // decoration: InputDecoration(
-              //   prefixIcon: Icon(Icons.view_list),
-              //   labelText: "Category",
-              //   hintText: "Write a category",
-              // ),
+              dropdownSearchDecoration: InputDecoration(
+                disabledBorder: InputBorder.none,
+                prefixIcon:  Icon(Icons.view_list),
+                alignLabelWithHint: true
+                )
+              // selectedItem: _selectedItem,
             ),
+            
+            //
             TextField(
+              controller: _dateController,
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.calendar_today),
                 labelText: "Pick a date",
@@ -52,6 +86,7 @@ class _AddNotePageState extends State<AddNotePage> {
               ),
             ),
             TextField(
+              controller: _descriptionController,
               keyboardType: TextInputType.multiline,
               minLines: 5,
               maxLines: 10,
